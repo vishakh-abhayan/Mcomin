fa6ad3278cc376519e08b34fc5a54e50ef77b96c
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-10-12 23:19:30 +0530

    app engine

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.engine(
   "hbs",
-  hbs({
+  hbs.engine({
     extname: "hbs",
     defualtLayout: "layout",
     layoutsDir: __dirname + "/views/layouts",
