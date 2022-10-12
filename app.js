1a5559ddc64e0578bb3c91b86eea438cc04f9b34
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-10-12 23:00:28 +0530

    app engine

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-//var hbs = require("express-handlebars");
+var hbs = require("express-handlebars");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -13,6 +13,15 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defualtLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
 
 app.use(logger("dev"));
 app.use(express.json());
